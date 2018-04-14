16473551112ecadef142e9af0ec707099a6518af
liuwenzhu <547485261@qq.com>
2018-04-14 10:16:10 +0800

    update
    Signed-off-by: liuwenzhu <547485261@qq.com>

--- version.js
+++ version.js
@@ -1,9 +1,9 @@
 {
 	"name":"test",
 	"version":"1.2.4",
-	"upDateUrl":"https://github.com/gitlwz/electron-version/raw/master/update/upgrade_tar.tar.gz",
-	"manifest":[
+	"manifest":[{
+		"version":"1.2.0",
 		"file":["index.js"],
 		"detail":["新增项目编辑功能"]
-	]
+	}]
 }
